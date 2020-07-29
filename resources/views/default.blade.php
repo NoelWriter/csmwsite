fe12830ef1a889ab6cfdecc6321e4ea948b7c82e
Noel Writer <schr0112@hz.nl>
2020-07-29 22:21:50 +0200

    Changed landing page

---
+++ resources/views/default.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Primary Meta Tags -->
+    <title>CSMW</title>
+    <meta name="title" content="CSMW - A Website by CosmicWolf">
+    <meta name="description" content="This website contains personal projects and information, including links to various social media and games such as Twitter, Discord and osu!. ">
+    <meta property="image" content="{{ url('/images/okami.jpg') }}">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <!-- Stylesheets -->
+    <link href="css/app.css" rel="stylesheet">
+
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('default')
+
+@section('content')
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <div class="title">
+            CSMW
+        </div>
+        <div class="m-b-md">
+            A website by CosmicWolf
+        </div>
+    </div>
+</div>
+@endsection
