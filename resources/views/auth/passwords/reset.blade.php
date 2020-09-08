06f0959c8b617b90261c160fa7ef78204510b552
Noel Writer <schr0112@hz.nl>
2020-09-08 19:51:58 +0200

    Fixed CSS issues

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,10 +1,10 @@
-@extends('layouts.app')
+@extends('default')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card bg-dark">
                 <div class="card-header">{{ __('Reset Password') }}</div>
 
                 <div class="card-body">
