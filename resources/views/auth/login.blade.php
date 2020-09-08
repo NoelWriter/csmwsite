9f19b83a50e6b83118c152e2bfe7339895503ae6
Noel Writer <schr0112@hz.nl>
2020-09-08 19:48:39 +0200

    Fixed CSS issues

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,10 +4,10 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card bg-dark">
                 <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body bg-dark">
+                <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
