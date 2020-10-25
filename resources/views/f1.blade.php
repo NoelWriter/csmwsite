db7070a3a868f3d85d200386ffdf7850957c92bc
Noel Writer <schr0112@hz.nl>
2020-10-25 15:10:50 +0100

    Added page for f1

--- resources/views/f1.blade.php
+++ resources/views/f1.blade.php
@@ -6,7 +6,7 @@
 
         <!-- Content Row -->
         <div class="row">
-            <iframe src="https://href.li/?http://ssoccerlive.xyz/player/ch10.php" marginwidth="0" marginheight="0" scrolling="no" allowfullscreen="allowfullscreen" sandbox="allow-same-origin allow-scripts allow-forms" width="100%" height="580" frameborder="0"></iframe>
+            <iframe src="http://ssoccerlive.xyz/player/ch10.php" marginwidth="0" marginheight="0" scrolling="no" allowfullscreen="allowfullscreen" sandbox="allow-same-origin allow-scripts allow-forms" width="100%" height="580" frameborder="0"></iframe>
         </div>
 
     </div>
