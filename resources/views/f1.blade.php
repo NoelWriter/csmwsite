0b6faefcff8a4ea1ea1bd88e493e7a1a0d09a55d
Noel Writer <schr0112@hz.nl>
2020-10-26 12:38:04 +0100

    added f1 page

--- resources/views/f1.blade.php
+++ resources/views/f1.blade.php
@@ -9,6 +9,11 @@
             <iframe marginwidth="0" marginheight="0" scrolling="no" allowfullscreen="allowfullscreen" src="https://sportscart.xyz/ch/scplayer-60.php" width="100%" height="580" frameborder="0" sandbox="allow-scripts allow-same-origin"></iframe>
         </div>
 
+        <div class="row align-content-center">
+            <h1 class="">This stream only works in the Firefox browser</h1>
+        </div>
+
+
     </div>
     <!-- /.container -->
 @endsection
