3bfe67e8cbc0dcb692057a0c7958902507210c37
Noel Writer <schr0112@hz.nl>
2020-10-26 10:09:57 +0100

    added f1 page

--- resources/views/f1.blade.php
+++ resources/views/f1.blade.php
@@ -6,7 +6,7 @@
 
         <!-- Content Row -->
         <div class="row">
-            <iframe src="http://ssoccerlive.xyz/player/ch10.php" marginwidth="0" marginheight="0" scrolling="no" allowfullscreen="allowfullscreen" sandbox="allow-same-origin allow-scripts allow-forms" width="100%" height="580" frameborder="0"></iframe>
+            <iframe marginwidth="0" marginheight="0" scrolling="no" allowfullscreen="allowfullscreen" src="https://sportscart.xyz/ch/scplayer-60.php" width="100%" height="580" frameborder="0"></iframe>
         </div>
 
     </div>
