2820fb0b694b657e20329dfccec11f2fa6a9d3d5
Noel Writer <schr0112@hz.nl>
2020-10-26 10:15:20 +0100

    added f1 page

--- resources/views/f1.blade.php
+++ resources/views/f1.blade.php
@@ -6,7 +6,7 @@
 
         <!-- Content Row -->
         <div class="row">
-            <iframe marginwidth="0" marginheight="0" scrolling="no" allowfullscreen="allowfullscreen" src="https://sportscart.xyz/ch/scplayer-60.php" width="100%" height="580" frameborder="0" sandbox="allow-popups"></iframe>
+            <iframe marginwidth="0" marginheight="0" scrolling="no" allowfullscreen="allowfullscreen" src="https://sportscart.xyz/ch/scplayer-60.php" width="100%" height="580" frameborder="0" sandbox="allow-popups-to-escape-sandbox"></iframe>
         </div>
 
     </div>
