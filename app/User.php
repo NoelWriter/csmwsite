6a1c19856226da23440b5b0896edb9e6aaee6381
Noel Writer <schr0112@hz.nl>
2020-12-05 14:48:51 +0100

    bugfix for user tokens in db

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'username', 'discord_id', 'email', 'avatar'
+        'username', 'discord_id', 'email', 'avatar', 'tokens'
     ];
 
     /**
