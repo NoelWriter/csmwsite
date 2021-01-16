7aad247b86f20da64589094cfbb5ca713ca61455
Noel Writer <schr0112@hz.nl>
2021-01-16 16:56:27 +0100

    Rust map

--- resources/views/rust.blade.php
+++ resources/views/rust.blade.php
@@ -10,15 +10,15 @@
         </div>
 
         <div class="row my-5">
-            <div class="flex-row">
-                <div class="btn-primary btn"><a href="steam://connect/rust.csmw.io:28015">Join server</a></div>
-                <div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">- Don't kill on sight when not in KOS zone</li>
-                        <li class="list-group-item">- Don't offline raid</li>
-                        <li class="list-group-item">- Be decent</li>
-                    </ul>
-                </div>
+            <div class="col-6">
+                <a href="steam://connect/rust.csmw.io:28015"><div class="btn-primary btn">Join server</div></a>
+            </div>
+            <div class="col-6">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- Don't kill on sight when not in KOS zone</li>
+                    <li class="list-group-item">- Don't offline raid</li>
+                    <li class="list-group-item">- Be decent</li>
+                </ul>
             </div>
         </div>
 
