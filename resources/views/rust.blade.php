fe8378aadfd9f3b023e14c1def4bd3fd4b26e210
Noel Writer <schr0112@hz.nl>
2021-01-16 16:28:17 +0100

    Rust map

--- resources/views/rust.blade.php
+++ resources/views/rust.blade.php
@@ -6,11 +6,11 @@
 
         <!-- Content Row -->
         <div class="row">
-            <iframe src="playrust.io/map/?Procedural Map_3500_17702#container" marginwidth="0" marginheight="0" scrolling="no" allowfullscreen="allowfullscreen" height="680" sandbox="allow-scripts allow-same-origin allow-forms" width="100%" frameborder="0"></iframe>
+            <iframe src="www.playrust.io/map/?Procedural Map_3500_17702" scrolling="no" allowfullscreen="allowfullscreen" height="680",width="100%"></iframe>
         </div>
 
         <div class="row">
-            <h3 class="text-center">*Stream is not hosted on this website</h3>
+
         </div>
 
 
